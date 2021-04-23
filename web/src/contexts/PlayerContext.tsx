434904039c0f0ac78ebf51c99dc1b1539b245d40
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2021-04-23 17:58:22 -0300

    controle de play aleatorio

--- web/src/contexts/PlayerContext.tsx
+++ web/src/contexts/PlayerContext.tsx
@@ -13,9 +13,11 @@ type PlayerContextData = {
   currentEpisodeIndex: number;
   isPlaying: boolean;
   isLooping: boolean;
+  isShuffling: boolean;
   play: (episode: Episode) => void;
   togglePlay: () => void;
   toggleLoop: () => void;
+  toggleShuffle: () => void;
   setPlayingState: (state: boolean) => void;
   playList: (list: Episode[], index: number) => void;
   playNext: () => void;
@@ -35,6 +37,7 @@ export function PlayerContextProvider ({ children }: PlayerContextProviderProps)
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
   const [isLooping, setIsLooping] = useState(false);
+  const [isShuffling, setIsShuffling] = useState(false);
 
   function play (episode: Episode) {
     setEpisodeList([episode]); //uma array com um unico episodio
@@ -56,6 +59,10 @@ export function PlayerContextProvider ({ children }: PlayerContextProviderProps)
     setIsLooping(!isLooping);
   }
 
+  function toggleShuffle () {
+    setIsShuffling(!isShuffling);
+  }
+
   function setPlayingState (state: boolean) {
     setIsPlaying(state);
   }
@@ -64,7 +71,10 @@ export function PlayerContextProvider ({ children }: PlayerContextProviderProps)
   const hasNext = (currentEpisodeIndex + 1) < episodeList.length;
 
   function playNext () {
-    if (hasNext) {
+    if (isShuffling) {
+      const nextRandomEpisodeIndex = Math.floor(Math.random() * episodeList.length);
+      setCurrentEpisodeIndex(nextRandomEpisodeIndex);
+    } else if (hasNext) {
       setCurrentEpisodeIndex(currentEpisodeIndex + 1);
     }
   }
@@ -86,8 +96,10 @@ export function PlayerContextProvider ({ children }: PlayerContextProviderProps)
         playPrevious,
         isPlaying,
         isLooping,
+        isShuffling,
         togglePlay,
         toggleLoop,
+        toggleShuffle,
         setPlayingState,
         hasNext,
         hasPrevious
