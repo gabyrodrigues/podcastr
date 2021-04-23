fbc6ce494867366aa4341f0f97357c3f9f179e68
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2021-04-23 16:15:37 -0300

    ajustando estrutura do context de player

--- web/src/contexts/PlayerContext.tsx
+++ web/src/contexts/PlayerContext.tsx
@@ -1,4 +1,4 @@
-import { createContext } from 'react';
+import { createContext, useState, ReactNode } from 'react';
 
 type Episode = {
   title: string;
@@ -19,3 +19,41 @@ type PlayerContextData = {
 };
 
 export const PlayerContext = createContext({} as PlayerContextData);
+
+type PlayerContextProviderProps = {
+  children: ReactNode; //quando o tipo pode ser qualquer coisa (div, string, number) por padrão é usado o tipo ReactNode
+}
+
+export function PlayerContextProvider ({ children }: PlayerContextProviderProps) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function play (episode: Episode) {
+    setEpisodeList([episode]); //uma array com um unico episodio
+    setCurrentEpisodeIndex(0); //força pra 0 de novo, pois como só tem um unico episodio, então esse index/episodio é o que precisa estar tocando
+    setIsPlaying(true);
+  }
+
+  function togglePlay () {
+    setIsPlaying(!isPlaying);
+  }
+
+  function setPlayingState (state: boolean) {
+    setIsPlaying(state);
+  }
+
+  return (
+    <PlayerContext.Provider
+      value={{
+        episodeList,
+        currentEpisodeIndex,
+        play,
+        isPlaying,
+        togglePlay,
+        setPlayingState
+      }}>
+      {children}
+    </PlayerContext.Provider>
+  );
+}
--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -5,30 +5,11 @@ import Header from '../components/Header';
 import Player from '../components/Player';
 
 import styles from '../styles/app.module.scss';
-import { PlayerContext } from '../contexts/PlayerContext';
-import { useState } from 'react';
+import { PlayerContextProvider } from '../contexts/PlayerContext';
 
 function MyApp({ Component, pageProps }) {
-  const [episodeList, setEpisodeList] = useState([]);
-  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  function play (episode) {
-    setEpisodeList([episode]); //uma array com um unico episodio
-    setCurrentEpisodeIndex(0); //força pra 0 de novo, pois como só tem um unico episodio, então esse index/episodio é o que precisa estar tocando
-    setIsPlaying(true);
-  }
-
-  function togglePlay () {
-    setIsPlaying(!isPlaying);
-  }
-
-  function setPlayingState (state: boolean) {
-    setIsPlaying(state);
-  }
-
-  return (
-    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState }}>
+  return(
+    <PlayerContextProvider>
       <div className={styles.wrapper}>
         <main>
           <Header />
@@ -36,7 +17,7 @@ function MyApp({ Component, pageProps }) {
         </main>
         <Player />
       </div>
-    </PlayerContext.Provider>
+    </PlayerContextProvider>
   );
 }
 
